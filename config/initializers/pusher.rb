8ba0585488ae90210fd236892114c63a24419ee2
zasmail <zasmail@utexas.edu>
2018-12-11 23:15:18 -0800

    Pusher for heroku

---
+++ config/initializers/pusher.rb
@@ -0,0 +1,8 @@
+require 'pusher'
+
+Pusher.app_id = '665987'
+Pusher.key = 'ffe05f6e970484c7c622'
+Pusher.secret = 'fddd909a824fe6484487'
+Pusher.cluster = 'us2'
+Pusher.logger = Rails.logger
+Pusher.encrypted = true
